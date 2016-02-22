1f91e3abe1d8ba5fde3312abf5303cc4476370b5
Jose Luis <jlds161089@gmail.com>
2016-02-21 23:41:39 -0500

    propiedades

---
+++ propiedades/todo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Task from './task'
+
+class ToDo extends React.Component {
+	render() {
+		return (
+			<ul>
+				<Task name='Introduction' done />
+				<Task name='Chapter 1 - First component' done />
+				<Task name='Chapter 2 - Properties' done={false} />
+			</ul>
+		)
+	}
+}
+
+export default ToDo
